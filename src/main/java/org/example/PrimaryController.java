8144cd793be7c1f54c9fcf0c37b7f29981d022ec
Jonathan Jeding <jonathan.jeding@gmail.com>
2021-04-15 23:55:01 +0200

    Added search function

--- src/main/java/org/example/PrimaryController.java
+++ src/main/java/org/example/PrimaryController.java
@@ -5,19 +5,16 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import org.library.Article;
-import org.library.Book;
 import org.library.LibraryOverseer;
 
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.util.ArrayList;
+import java.util.Objects;
 
 public class PrimaryController {
     public Label homeButton;
@@ -27,6 +24,9 @@ public class PrimaryController {
     public VBox libView;
     public StackPane libStackPane;
     public BorderPane mainPane;
+    public TextField searchBar;
+    public Button searchButton;
+    private Connection connection;
 
     /**
      * Switches scene to login
@@ -39,7 +39,8 @@ public class PrimaryController {
     }
 
     public void initialize() {
-        for (Article article : allArticles()) {
+        connection = LibraryOverseer.createDBConnection();
+        for (Article article : LibraryOverseer.allArticles(connection)) {
             BorderPane borderPane = new BorderPane();
             Label label = new Label(article.getTitle());
             Button button = new Button("Låna");
@@ -47,41 +48,24 @@ public class PrimaryController {
             BorderPane.setAlignment(label, Pos.CENTER_LEFT);
             borderPane.setRight(button);
             libView.getChildren().add(borderPane);
+        }
+    }
 
-
+    @FXML
+    public void search() {
+        libView.getChildren().clear();
+        for (Article article : Objects.requireNonNull(LibraryOverseer.searchArticle(searchBar.textProperty().getValue().toLowerCase().strip(), connection))) {
+            BorderPane borderPane = new BorderPane();
+            Label label = new Label(article.getTitle());
+            Button button = new Button("Låna");
+            borderPane.setLeft(label);
+            BorderPane.setAlignment(label, Pos.CENTER_LEFT);
+            borderPane.setRight(button);
+            libView.getChildren().add(borderPane);
         }
     }
     // Lite hjälp med detta error. Kan inte ta bort module-info.java utan error :)
     // Går annars att lösa genom att dra in allt under example (?)
 
-    /**
-     * Get all articles in the library
-     *
-     * @return ArrayList of articles
-     */
-    public ArrayList<Article> allArticles() {
-        ArrayList<Article> articles = new ArrayList<>();
-        Connection conn = LibraryOverseer.createDBConnection();
 
-        try {
-            assert conn != null;
-            Statement statement = conn.createStatement();
-            ResultSet rs = statement.executeQuery("select * from artikel");
-            while (rs.next()) {
-                int id = rs.getInt("artikelID");
-                String title = rs.getString("titel");
-                int year = rs.getInt("ar");
-                String isbn = rs.getString("ISBN");
-                String[] authors = {"Not yet"};
-                Double physical_location = rs.getDouble("fysiskPlats");
-                int inStock = rs.getInt("antal");
-
-                Book book = new Book(id, title, year, isbn, authors, physical_location, inStock);
-                articles.add(book);
-            }
-        } catch (Exception e) {
-            System.out.println("Something went wrong in allArticles()");
-        }
-        return articles;
-    }
 }
--- src/main/java/org/library/LibraryOverseer.java
+++ src/main/java/org/library/LibraryOverseer.java
@@ -1,9 +1,13 @@
 package org.library;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.io.InputStream;
+import java.io.Reader;
+import java.math.BigDecimal;
+import java.net.URL;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Objects;
 
 /**
  *
@@ -14,6 +18,7 @@ public class LibraryOverseer { // Database currently incomplete. Might rename cl
     private static final String username = "root";
     private static final String password = test.getPwd();
 
+    static ArrayList<Article> articles = new ArrayList<>();
     /**
      * @return Connection from variables in LibraryOverseer
      */
@@ -27,6 +32,58 @@ public class LibraryOverseer { // Database currently incomplete. Might rename cl
             System.out.println("Cannot create database connection");
             e.printStackTrace();
             return null;
+        }
+    }
+    /**
+     * Get all articles in the library
+     *
+     * @return ArrayList of articles
+     */
+    public static ArrayList<Article> allArticles(Connection connection) {
+
+        try {
+            assert connection != null;
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("select * from artikel");
+            rsGet(articles, rs);
+            System.out.println(articles);
+        } catch (Exception e) {
+            System.out.println("Something went wrong in allArticles()");
+        }
+
+        return articles;
+    }
+    
+    public static ArrayList<Article> searchArticle(String statement, Connection connection) {
+        statement = "%" + statement + "%";
+        ArrayList<Article> articleArrayList= new ArrayList<>();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("select * from artikel where titel like ?");
+            preparedStatement.setString(1, statement);
+            System.out.println(preparedStatement);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            rsGet(articleArrayList, resultSet);
+            System.out.println(articleArrayList);
+            articles = articleArrayList;
+            return articles;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            return null;
+        }
+    }
+
+    private static void rsGet(ArrayList<Article> articleList, ResultSet resultSet) throws SQLException {
+        while (resultSet.next()){
+            int id = resultSet.getInt("artikelID");
+            String title = resultSet.getString("titel");
+            int year = resultSet.getInt("ar");
+            String isbn = resultSet.getString("ISBN");
+            String[] authors = {"Not yet"};
+            Double physical_location = resultSet.getDouble("fysiskPlats");
+            int inStock = resultSet.getInt("antal");
+
+            Book book = new Book(id, title, year, isbn, authors, physical_location, inStock);
+            articleList.add(book);
         }
     }
 }
