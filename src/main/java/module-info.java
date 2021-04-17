294ccd9a47182707ca1393b80893a33df94e9a0a
Maximilian Wendel <maximilian.wendel@mwtdev.se>
2021-04-17 20:08:09 +0200

    Added bcrypt to module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.example {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires bcrypt;
 
     opens org.example to javafx.fxml;
     exports org.example;
