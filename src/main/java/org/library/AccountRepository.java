ed22eb1e4a0471334fffca92a33dc233079e8192
Maximilian Wendel <maximilian.wendel@mwtdev.se>
2021-04-24 12:15:15 +0200

    Updated account repository documentation and added create method

--- src/main/java/org/library/AccountRepository.java
+++ src/main/java/org/library/AccountRepository.java
@@ -6,6 +6,12 @@ public class AccountRepository extends Repository<Account> {
         super("account", database);
     }
 
+    /**
+     * Get an account by id.
+     * @param id Account id.
+     * @return Account with the provided id.
+     * @throws Exception If no account with the provided id exists.
+     */
     public Account getByID(int id) throws Exception {
         return super.getByID(id, rs -> new Account(
                 rs.getString("givenName"),
@@ -13,4 +19,27 @@ public class AccountRepository extends Repository<Account> {
                 rs.getString("email")
         ));
     }
+
+    /**
+     * Attempts to create a new account.
+     * @param givenName A valid given name.
+     * @param familyName A valid family name.
+     * @param email A valid email.
+     * @param passwordHash A bcrypt password hash.
+     * @return A boolean indicating if the operation was successful.
+     * @throws Exception
+     */
+    public boolean create(String givenName, String familyName, String email, String passwordHash) throws Exception {
+        return super.create(new String[]{"givenName", "familyName", "email", "passwordHash"}, configuration -> {
+            configuration.setString(1, givenName);
+            configuration.setString(2, familyName);
+            configuration.setString(3, email);
+            configuration.setString(4, passwordHash);
+        });
+    }
+
+    // TODO: Get account by email
+
+    // TODO: Get all accounts
+
 }
