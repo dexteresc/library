ed5ceee6efe92a182f9b9debb0b2de684812c71b
Maximilian Wendel <maximilian.wendel@mwtdev.se>
2021-04-25 13:36:56 +0200

    Added findAll method to repository

--- src/main/java/org/library/Repository.java
+++ src/main/java/org/library/Repository.java
@@ -2,6 +2,7 @@ package org.library;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
 public abstract class Repository<T> {
     // Produces general prepared statement query-strings.
@@ -9,6 +10,10 @@ public abstract class Repository<T> {
         // Creates a get by id statement for the provided table name.
         static String find(String table, String attribute) {
             return "SELECT * FROM " + table + " WHERE " + attribute + " = ? LIMIT 1";
+        }
+
+        static String findAll(String table, String condition, int limit, int offset, String[] orderBy) {
+            return "SELECT * FROM " + table + " WHERE " + condition + " ORDER BY " + String.join(", ", orderBy) + " OFFSET " + offset + " ROWS" + " LIMIT " + limit;
         }
 
         // Creates an insert into statement for the provided table name and attributes.
@@ -67,6 +72,24 @@ public abstract class Repository<T> {
         String statement = Statements.find(table, attribute);
         return database.query(statement, preparedStatement -> preparedStatement.setInt(1, value), transformation);
     }
+
+    public ArrayList<T> findAll(String condition, String[] orderBy, int limit, int offset, Database.Configuration<PreparedStatement> configuration, Transformation<ResultSet, T> transformation) throws Exception {
+        String statement = Statements.findAll(table, condition, limit, offset, orderBy);
+        return database.query(statement, configuration, rs -> {
+            ArrayList<T> items = new ArrayList<>();
+
+            // Add the first item, as it will be missed by the loop due to the database query implementation.
+            items.add(transformation.materialize(rs));
+
+            // Collect entities from result set.
+            while (rs.next()) {
+                items.add(transformation.materialize(rs));
+            }
+
+            return items;
+        });
+    }
+
 
     /**
      * Attempt to create an entity.
