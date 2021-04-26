2dad2a825d4c8b43a32eeccbad494742fc335273
Maximilian Wendel <maximilian.wendel@mwtdev.se>
2021-04-26 17:06:59 +0200

    Added shorter repository findAll method

--- src/main/java/org/library/Repository.java
+++ src/main/java/org/library/Repository.java
@@ -16,8 +16,12 @@ public abstract class Repository<T> {
             return "SELECT * FROM " + table + " WHERE " + attribute + " = ? LIMIT 1";
         }
 
-        static String findAll(String table, String condition, int limit, int offset, String[] orderBy) {
-            return "SELECT * FROM " + table + " WHERE " + condition + " ORDER BY " + String.join(", ", orderBy) + " OFFSET " + offset + " ROWS" + " LIMIT " + limit;
+        static String findAll(String table, String condition, int limit, int offset, String orderBy) {
+            return "SELECT * FROM " + table + " WHERE " + condition + " ORDER BY " + orderBy + " OFFSET " + offset + " ROWS" + " LIMIT " + limit;
+        }
+
+        static String findAll(String table, int limit, int offset, String orderBy) {
+            return "SELECT * FROM " + table + " ORDER BY " + orderBy + " OFFSET " + offset + " ROWS" + " LIMIT " + limit;
         }
 
         // Creates an insert into statement for the provided table name and attributes.
@@ -89,9 +93,36 @@ public abstract class Repository<T> {
      * @return A list of matched entities.
      * @throws Exception If the transformation failed, or a general database error occurred.
      */
-    public ArrayList<T> findAll(String condition, String[] orderBy, int limit, int offset, Database.Configuration<PreparedStatement> configuration, Transformation<ResultSet, T> transformation) throws Exception {
+    public ArrayList<T> findAll(String condition, String orderBy, int limit, int offset, Database.Configuration<PreparedStatement> configuration, Transformation<ResultSet, T> transformation) throws Exception {
         String statement = Statements.findAll(table, condition, limit, offset, orderBy);
         return database.query(statement, configuration, rs -> {
+            ArrayList<T> items = new ArrayList<>();
+
+            // Add the first item, as it will be missed by the loop due to the database query implementation.
+            items.add(transformation.materialize(rs));
+
+            // Collect entities from result set.
+            while (rs.next()) {
+                items.add(transformation.materialize(rs));
+            }
+
+            return items;
+        });
+    }
+
+    /**
+     * Find all entities matching a given condition.
+     * @param orderBy Attribute to order matched entities by.
+     * @param limit Max number of entities to return.
+     * @param offset Number of matched entities to skip.
+     * @param transformation A lambda expression for transforming a result set into the entity.
+     * @implNote Transformation expression should only transform a single entity as it is run on all matches automagically.
+     * @return A list of matched entities.
+     * @throws Exception If the transformation failed, or a general database error occurred.
+     */
+    public ArrayList<T> findAll(String orderBy, int limit, int offset, Transformation<ResultSet, T> transformation) throws Exception {
+        String statement = Statements.findAll(table, limit, offset, orderBy);
+        return database.query(statement, preparedStatement -> {}, rs -> {
             ArrayList<T> items = new ArrayList<>();
 
             // Add the first item, as it will be missed by the loop due to the database query implementation.
