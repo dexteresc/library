fb4d62b1a158ee2cc4f3bf2aaa2388af0375c34f
Maximilian Wendel <maximilian.wendel@mwtdev.se>
2021-05-03 15:57:54 +0200

    Added customer class

---
+++ src/main/java/org/library/Customer.java
@@ -0,0 +1,9 @@
+package org.library;
+
+public class Customer extends Account {
+
+    public Customer(int id, String givenName, String familyName, String email) {
+        super(id, givenName, familyName, email);
+    }
+
+}
