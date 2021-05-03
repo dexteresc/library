4ba557ec53f5f69f4fc94ece484ca1af9a132723
Maximilian Wendel <maximilian.wendel@mwtdev.se>
2021-05-03 16:11:09 +0200

    Updated customer class to include customer type

--- src/main/java/org/library/Customer.java
+++ src/main/java/org/library/Customer.java
@@ -1,9 +1,15 @@
 package org.library;
 
 public class Customer extends Account {
+    private CustomerType customerType;
 
-    public Customer(int id, String givenName, String familyName, String email) {
-        super(id, givenName, familyName, email);
+    public Customer(Account account, CustomerType customerType) {
+        super(account.getID(), account.getGivenName(), account.getFamilyName(), account.getEmail());
+
+        this.customerType = customerType;
     }
 
+    public CustomerType getCustomerType() {
+        return customerType;
+    }
 }
