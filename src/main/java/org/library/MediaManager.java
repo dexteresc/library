95321d61b5a3cfb047bb3025ba94471dae1cb5f7
Maximilian Wendel <maximilian.wendel@mwtdev.se>
2021-05-08 16:22:30 +0200

    Added method for creating and updating books

--- src/main/java/org/library/MediaManager.java
+++ src/main/java/org/library/MediaManager.java
@@ -1,13 +1,17 @@
 package org.library;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 public class MediaManager {
 
     // Statements
     private static final String SELECT_MEDIA_STATEMENT = "SELECT * FROM media WHERE id = ? LIMIT 1";
-    private static final String UPDATE_MEDIA_STATEMENT = "UPDATE media SET title = ?, category = ?, classification = ?, summary = ?, publisher = ?, publishing_date = ? WHERE id = ? LIMIT 1";
-    private static final String CREATE_MEDIA_STATEMENT = "INSERT INTO media (title, category, classification, summary, publisher, publishing_date) VALUES (?, ?, ?, ?, ?, ?)";
+    private static final String UPDATE_MEDIA_STATEMENT = "UPDATE media SET title = ?, classification = ?, summary = ?, location = ?, publishing_date = ? WHERE id = ? LIMIT 1";
+    private static final String CREATE_MEDIA_STATEMENT = "INSERT INTO media (title, classification, summary, location, publishing_date) VALUES (?, ?, ?, ?, ?)";
+
+    // Book statements
+    private static final String CREATE_BOOK_STATEMENT = "INSERT INTO book (media_id, isbn, publisher) VALUES (?, ?, ?)";
+    private static final String UPDATE_BOOK_STATEMENT = "UPDATE book SET isbn = ?, publisher = ? WHERE id = ?";
 
     private final Database database;
 
@@ -15,16 +19,30 @@ public class MediaManager {
         this.database = database;
     }
 
-    public void createMedia(String title, String category, String classification, String summary, String publisher, Date publishingDate) throws Exception {
-        database.insert(CREATE_MEDIA_STATEMENT)
-                .configure(title, category, classification, summary, publisher, publishingDate)
-                .execute();
+    private Long createMedia(String title, String classification, String summary, String location, LocalDate publishingDate) throws Exception {
+        return database.insert(CREATE_MEDIA_STATEMENT)
+                .configure(title, classification, summary, location, publishingDate)
+                .executeQuery();
     }
 
-    public void updateMedia(Media media) throws Exception {
+    private void updateMedia(Media media) throws Exception {
         database.update(UPDATE_MEDIA_STATEMENT)
-                .configure(media.getTitle(), media.getCategory(), media.getClassification(), media.getSummary(), media.getPublisher(), media.getPublishingDate(), media.getId())
+                .configure(media.getTitle(), media.getClassification(), media.getSummary(), media.getLocation(), media.getPublishingDate(), media.getId())
                 .execute();
     }
 
+    public void createBook(String title, String classification, String summary, String location, LocalDate publishingDate, String isbn, String publisher) throws Exception {
+        Long id = this.createMedia(title, classification, summary, location, publishingDate);
+        database.insert(CREATE_BOOK_STATEMENT)
+                .configure(id, isbn, publisher)
+                .execute();
+    }
+
+    // TODO: This is inefficient. We should batch-process the book update alongside the media update.
+    public void updateBook(Book book) throws Exception {
+        this.updateMedia(book);
+        database.update(UPDATE_BOOK_STATEMENT)
+                .configure(book.getIsbn(), book.getPublisher(), book.getId())
+                .execute();
+    }
 }
