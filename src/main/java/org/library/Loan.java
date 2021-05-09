7349cd9a9be3912fc9dcf3396eba87fb54902601
Maximilian Wendel <maximilian.wendel@mwtdev.se>
2021-05-09 14:59:56 +0200

    Added loan getters

--- src/main/java/org/library/Loan.java
+++ src/main/java/org/library/Loan.java
@@ -24,4 +24,28 @@ public class Loan {
     public Loan(ResultSet resultSet) throws SQLException {
         this(resultSet.getLong("id"), resultSet.getLong("customer_id"), resultSet.getLong("media_item_id"), resultSet.getObject("borrowed_at", LocalDate.class), resultSet.getObject("return_by", LocalDate.class), resultSet.getObject("returned_at", LocalDate.class));
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public Long getMediaItemId() {
+        return mediaItemId;
+    }
+
+    public LocalDate getBorrowedAt() {
+        return borrowedAt;
+    }
+
+    public LocalDate getReturnBy() {
+        return returnBy;
+    }
+
+    public LocalDate getReturnedAt() {
+        return returnedAt;
+    }
 }
