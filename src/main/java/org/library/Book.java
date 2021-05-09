bf4c9118e08ef3052b1ae0e97de522b0d968fec8
Maximilian Wendel <maximilian.wendel@mwtdev.se>
2021-05-09 13:59:44 +0200

    Updated book to get location instead of category

--- src/main/java/org/library/Book.java
+++ src/main/java/org/library/Book.java
@@ -11,14 +11,14 @@ public class Book extends Media {
     private String isbn;
     private String publisher;
 
-    public Book(Long id, String title, String category, String classification, String summary, LocalDate publishingDate, String publisher, String isbn) {
-        super(id, title, category, classification, summary, publishingDate);
+    public Book(Long id, String title, String classification, String summary, String location, LocalDate publishingDate, String publisher, String isbn) {
+        super(id, title, classification, summary, location, publishingDate);
         this.isbn = isbn;
         this.publisher = publisher;
     }
 
     public Book(ResultSet resultSet) throws SQLException {
-        this(resultSet.getLong("id"), resultSet.getString("title"), resultSet.getString("category"), resultSet.getString("classification"), resultSet.getString("summary"), resultSet.getObject("publishing_date", LocalDate.class), resultSet.getString("isbn"), resultSet.getString("publisher"));
+        this(resultSet.getLong("id"), resultSet.getString("title"), resultSet.getString("classification"), resultSet.getString("summary"), resultSet.getString("location"), resultSet.getObject("publishing_date", LocalDate.class), resultSet.getString("isbn"), resultSet.getString("publisher"));
     }
 
     public String getIsbn() {
