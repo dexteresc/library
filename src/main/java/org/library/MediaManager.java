86e6de2f82c856b9e931cb55694ee31f423ee23f
Maximilian Wendel <maximilian.wendel@mwtdev.se>
2021-05-10 11:20:56 +0200

    Updated book search query to use FULLTEXT index

--- src/main/java/org/library/MediaManager.java
+++ src/main/java/org/library/MediaManager.java
@@ -14,7 +14,7 @@ public class MediaManager {
     private static final String SELECT_BOOK_BY_ID_STATEMENT = "SELECT *, (SELECT COUNT(*) FROM media_author WHERE media_id = media.id) AS author_count FROM media INNER JOIN book ON book.media_id = media.id INNER JOIN media_author ON media_author.media_id = media.id INNER JOIN author ON author.id = media_author.author_id WHERE id = ?";
     private static final String CREATE_BOOK_STATEMENT = "INSERT INTO book (media_id, isbn, publisher) VALUES (?, ?, ?)";
     private static final String UPDATE_BOOK_STATEMENT = "UPDATE book SET isbn = ?, publisher = ? WHERE id = ? LIMIT 1";
-    private static final String SEARCH_BOOK_STATEMENT = "SELECT *, (SELECT COUNT(*) FROM media_author WHERE media_id = media.id) AS author_count FROM media INNER JOIN book ON book.media_id = media.id INNER JOIN media_author ON media_author.media_id = media.id INNER JOIN author ON author.id = media_author.author_id WHERE media.title LIKE ?";
+    private static final String SEARCH_BOOK_STATEMENT = "SELECT *, (SELECT COUNT(*) FROM media_author WHERE media_id = media.id) AS author_count FROM media INNER JOIN book ON book.media_id = media.id INNER JOIN media_author ON media_author.media_id = media.id INNER JOIN author ON author.id = media_author.author_id WHERE MATCH(media.title, media.summary, media.classification) AGAINST(?) OR MATCH(author.given_name, author.family_name) AGAINST(?)";
 
     // Media item statements
     private static final String SELECT_MEDIA_ITEMS_BY_MEDIA_ID_STATEMENT = "SELECT * FROM media_item INNER JOIN media_type ON media_item.media_type_id = media_type.id WHERE media_id = ?";
@@ -77,7 +77,7 @@ public class MediaManager {
 
     public List<Book> searchBook(String query) throws Exception {
         return database.select(SEARCH_BOOK_STATEMENT, Book.class)
-                .configure("%" + query + "%")
+                .configure(query, query)
                 .fetchAll(Book::new);
     }
 
