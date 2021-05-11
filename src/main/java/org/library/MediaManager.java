ec314e45cbc42bc789172b366275d913e868c5a2
Maximilian Wendel <maximilian.wendel@mwtdev.se>
2021-05-11 15:46:18 +0200

    Added methods for creating, getting, searching and updating movie

--- src/main/java/org/library/MediaManager.java
+++ src/main/java/org/library/MediaManager.java
@@ -1,24 +1,25 @@
 package org.library;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MediaManager {
 
     // Statements
-    private static final String SELECT_MEDIA_STATEMENT = "SELECT * FROM media WHERE id = ? LIMIT 1";
     private static final String UPDATE_MEDIA_STATEMENT = "UPDATE media SET title = ?, classification = ?, summary = ?, location = ?, publishing_date = ? WHERE id = ? LIMIT 1";
     private static final String CREATE_MEDIA_STATEMENT = "INSERT INTO media (title, classification, summary, location, publishing_date) VALUES (?, ?, ?, ?, ?)";
+    private static final String DELETE_MEDIA_STATEMENT = "DELETE FROM media WHERE id = ?";
 
     // Book statements
     private static final String SELECT_BOOK_BY_ID_STATEMENT = "SELECT *, (SELECT COUNT(*) FROM media_author WHERE media_id = media.id) AS author_count FROM media INNER JOIN book ON book.media_id = media.id INNER JOIN media_author ON media_author.media_id = media.id INNER JOIN author ON author.id = media_author.author_id WHERE id = ?";
     private static final String CREATE_BOOK_STATEMENT = "INSERT INTO book (media_id, isbn, publisher) VALUES (?, ?, ?)";
-    private static final String UPDATE_BOOK_STATEMENT = "UPDATE book SET isbn = ?, publisher = ? WHERE id = ? LIMIT 1";
+    private static final String UPDATE_BOOK_STATEMENT = "UPDATE book SET isbn = ?, publisher = ? WHERE media_id = ? LIMIT 1";
     private static final String SEARCH_BOOK_STATEMENT = "SELECT *, (SELECT COUNT(*) FROM media_author WHERE media_id = media.id) AS author_count FROM media INNER JOIN book ON book.media_id = media.id INNER JOIN media_author ON media_author.media_id = media.id INNER JOIN author ON author.id = media_author.author_id WHERE MATCH(media.title, media.summary, media.classification) AGAINST(? IN NATURAL LANGUAGE MODE) OR MATCH(author.given_name, author.family_name) AGAINST(? IN NATURAL LANGUAGE MODE) OR MATCH(book.isbn, book.publisher) AGAINST(? IN NATURAL LANGUAGE MODE)";
 
     // Movie statements
     private static final String SELECT_MOVIE_BY_ID_STATEMENT = "SELECT *, (SELECT COUNT(*) FROM media_actor WHERE media_id = media.id) AS actor_count FROM media INNER JOIN movie ON movie.media_id = media.id INNER JOIN media_actor ON media_actor.media_id = media.id INNER JOIN actor ON actor.id = media_actor.actor_id WHERE id = ?";
     private static final String CREATE_MOVIE_STATEMENT = "INSERT INTO movie (media_id, director, age_rating, production_country) VALUES (?, ?, ?)";
-    private static final String UPDATE_MOVIE_STATEMENT = "UPDATE movie SET director = ?, age_rating = ?, production_country = ? WHERE id = ? LIMIT 1";
+    private static final String UPDATE_MOVIE_STATEMENT = "UPDATE movie SET director = ?, age_rating = ?, production_country = ? WHERE media_id = ? LIMIT 1";
     private static final String SEARCH_MOVIE_STATEMENT = "SELECT *, (SELECT COUNT(*) FROM media_actor WHERE media_id = media.id) AS actor_count FROM media INNER JOIN movie ON movie.media_id = media.id INNER JOIN media_actor ON media_actor.media_id = media.id INNER JOIN actor ON actor.id = media_actor.actor_id WHERE MATCH(media.title, media.summary, media.classification) AGAINST(? IN NATURAL LANGUAGE MODE) OR MATCH(actor.given_name, actor.family_name) AGAINST(? IN NATURAL LANGUAGE MODE) OR MATCH(movie.director, movie.production_country) AGAINST(? IN NATURAL LANGUAGE MODE)";
 
     // TODO: Author statements
@@ -86,6 +87,49 @@ public class MediaManager {
         return database.select(SEARCH_BOOK_STATEMENT, Book.class)
                 .configure(query, query, query)
                 .fetchAll(Book::new);
+    }
+
+    public void deleteMediaById(Long id) throws Exception {
+        database.delete(DELETE_MEDIA_STATEMENT)
+                .configure(id)
+                .execute();
+    }
+
+    public Movie getMovieById(Long id) throws Exception {
+        return database.select(SELECT_MOVIE_BY_ID_STATEMENT, Movie.class)
+                .configure(id)
+                .fetch(Movie::new);
+    }
+
+    public void createMovie(Movie movie) throws Exception {
+        this.createMedia(movie);
+        database.insert(CREATE_MOVIE_STATEMENT)
+                .configure(movie.getId(), movie.getDirector(), movie.getAgeRating(), movie.getProductionCountry())
+                .execute();
+    }
+
+    public void updateMovie(Movie movie) throws Exception {
+        this.updateMedia(movie);
+        database.update(UPDATE_MOVIE_STATEMENT)
+                .configure(movie.getDirector(), movie.getAgeRating(), movie.getProductionCountry(), movie.getId())
+                .execute();
+    }
+
+    public List<Movie> searchMovie(String query) throws Exception {
+        return database.select(SEARCH_MOVIE_STATEMENT, Movie.class)
+                .configure(query, query, query)
+                .fetchAll(Movie::new);
+    }
+
+    public List<Media> searchMedia(String query) throws Exception {
+        List<Media> mediaList = new ArrayList<>();
+        mediaList.addAll(
+                this.searchBook(query)
+        );
+        mediaList.addAll(
+                this.searchMovie(query)
+        );
+        return mediaList;
     }
 
 }
