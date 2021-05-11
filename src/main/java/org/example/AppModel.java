f6d4ffa5cbe9caaf210d8cd8a127a2679fbccb1b
Maximilian Wendel <maximilian.wendel@mwtdev.se>
2021-05-11 17:07:37 +0200

    Added search and loan models

--- src/main/java/org/example/AppModel.java
+++ src/main/java/org/example/AppModel.java
@@ -1,26 +1,46 @@
 package org.example;
 
-import org.library.AccountManager;
-import org.library.AuthenticationModel;
-import org.library.Database;
+import org.library.*;
 
 public final class AppModel {
 
     private Database database;
 
+    // Managers
+    private AccountManager accountManager;
+    private MediaManager mediaManager;
+    private LoanManager loanManager;
+
     // Models
     private AuthenticationModel authenticationModel;
+    private SearchModel searchModel;
+    private LoanModel loanModel;
 
     public AppModel(Database database) {
         this.database = database;
 
+        // Managers
+        this.accountManager = new AccountManager(this.database);
+        this.mediaManager = new MediaManager(this.database);
+        this.loanManager = new LoanManager(this.database);
+
         // Models
-        this.authenticationModel = new AuthenticationModel(new AccountManager(this.database));
+        this.authenticationModel = new AuthenticationModel(this.accountManager);
+        this.searchModel = new SearchModel(this.mediaManager);
+        this.loanModel = new LoanModel(this.loanManager);
     }
 
     // Models
     public AuthenticationModel getAuthenticationModel() {
         return authenticationModel;
+    }
+
+    public SearchModel getSearchModel() {
+        return searchModel;
+    }
+
+    public LoanModel getLoanModel() {
+        return loanModel;
     }
 
 }
