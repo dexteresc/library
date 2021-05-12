ad9bb85beed98b2a1f71989af7d069006aeab7d9
Maximilian Wendel <maximilian.wendel@mwtdev.se>
2021-05-12 19:53:57 +0200

    Added todo for media manager

--- src/main/java/org/library/MediaManager.java
+++ src/main/java/org/library/MediaManager.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
+// TODO: Split into book manager, movie manager, media manager, media item manager, author manager and actor manager.
 public class MediaManager {
     private static final Logger logger = LogManager.getLogger();
 
