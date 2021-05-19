5df5fcdd8456efa5ddd445437e4f4ad041e9a636
jonat <jonathan.jeding@gmail.com>
2021-05-19 16:24:39 +0200

    Admin text area added

--- src/main/java/org/example/AdminController.java
+++ src/main/java/org/example/AdminController.java
@@ -2,6 +2,7 @@ package org.example;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
@@ -14,59 +15,120 @@ import org.library.Book;
 import org.library.Media;
 import org.w3c.dom.Text;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
-public class AdminController implements Initializable{
+public class AdminController implements Initializable {
+    public Button saveButton;
     AdminModel adminModel;
     @FXML
     Button backButton;
     @FXML
     VBox informationVBox;
 
+    String title;
+    List<Author> authorList;
+    TextArea titleArea;
+    VBox authorVBox;
+    TextArea descriptionArea;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         // get adminModel
-        this.adminModel = App.getAppModel().getAdminModel();
-        Media media = adminModel.getMedia();
-
+        adminModel = App.getAppModel().getAdminModel();
         informationVBox.getChildren().clear();
+
+
+        // Back Button
+        backButton.setOnAction(actionEvent -> {
+            try {
+                App.setRoot("primary");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
 
         // Create module
 
         // Title
-        TextArea titleArea = new TextArea();
+        titleArea = new TextArea();
         titleArea.setWrapText(true);
-        titleArea.setText(media.getTitle());
         HBox titleBox = new HBox();
         titleBox.getChildren().add(new Label("Title:"));
         titleBox.getChildren().add(titleArea);
         informationVBox.getChildren().add(titleBox);
 
-/*
+        // Description
+        HBox descriptionHBox = new HBox();
+        descriptionHBox.getChildren().add(new Label("Description:"));
+        descriptionArea = new TextArea();
+        descriptionHBox.getChildren().add(descriptionArea);
+        informationVBox.getChildren().add(descriptionHBox);
+
         // Authors
-        if (media instanceof Book){
-            VBox authorVBox = new VBox();
-            List<Author> authorList = ((Book) media).getAuthors();
+
+        authorVBox = new VBox();
+
+        informationVBox.getChildren().add(new Label("Authors:"));
+        HBox authorNewHBox = new HBox();
+        informationVBox.getChildren().add(authorVBox);
+        informationVBox.getChildren().add(new Label("New author:"));
+        authorNewHBox.getChildren().add(new Label("Given name:"));
+        TextField givenNameField = new TextField();
+        authorNewHBox.getChildren().add(givenNameField);
+        authorNewHBox.getChildren().add(new Label("Family: name"));
+        TextField familyNameField = new TextField();
+        authorNewHBox.getChildren().add(familyNameField);
+        informationVBox.getChildren().add(authorNewHBox);
+
+        // Save Button
+        saveButton.setOnAction(actionEvent -> {
+            if (!(titleArea.textProperty().getValue().equals(title))) {
+                // TODO: 5/19/2021 Change title
+            }
+
+            if (!(givenNameField.textProperty().getValue().equals("")) || !(familyNameField.textProperty().getValue().equals(""))) {
+                // TODO: Add or create new author
+            }
+
+        });
+
+        if (adminModel.getMedia() == null) {
+            createMedia();
+        } else {
+            editMedia();
+        }
+    }
+
+    public void editMedia() {
+
+        Media media = adminModel.getMedia();
+
+        titleArea.setText(media.getTitle());
+        descriptionArea.setText(media.getSummary());
+        if (media instanceof Book) {
+            authorList = ((Book) media).getAuthors();
             for (Author author :
                     authorList) {
                 HBox authorBox = new HBox();
-                TextField givenName = new TextField(author.getGivenName());
-                TextField familyName = new TextField(author.getFamilyName());
-                authorBox.getChildren().add(givenName);
-                authorBox.getChildren().add(familyName);
+                Label authorName = new Label(author.getGivenName() + " " + author.getFamilyName());
+                Button deleteAuthorButton = new Button("delete");
+                deleteAuthorButton.setOnAction(actionEvent -> {
+                    // TODO: 5/19/2021 Delete author
+                });
+                deleteAuthorButton.getStyleClass().add("coloredButton");
+                authorBox.getChildren().add(authorName);
+                authorBox.getChildren().add(deleteAuthorButton);
                 authorVBox.getChildren().add(authorBox);
             }
-            informationVBox.getChildren().add(new Label("Authors:"));
-            informationVBox.getChildren().add(authorVBox);
         }
-
- */
-
-
-
-
-
     }
+
+    public void createMedia() {
+    }
+
 }
