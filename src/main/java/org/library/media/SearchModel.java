45d2bfe4d306f5900a787c838cdbecf7d50dfdf9
Maximilian Wendel <maximilian.wendel@mwtdev.se>
2021-05-19 15:08:24 +0200

    Removed redundant imports

--- src/main/java/org/library/media/SearchModel.java
+++ src/main/java/org/library/media/SearchModel.java
@@ -5,8 +5,6 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.library.media.Media;
-import org.library.media.MediaManager;
 
 import java.util.ArrayList;
 
