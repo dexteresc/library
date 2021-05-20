b7604225ce015898cbfb6bd168327872938617b6
Maximilian Wendel <maximilian.wendel@mwtdev.se>
2021-05-20 16:30:02 +0200

    Added javadoc comments for actor class

--- src/main/java/org/library/media/Actor.java
+++ src/main/java/org/library/media/Actor.java
@@ -3,8 +3,19 @@ package org.library.media;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+/**
+ * Actor representing an actor in a movie.
+ *
+ * @see org.library.media.Movie
+ */
 public class Actor extends Person {
 
+    /**
+     * Creates a new actor instance from a result set.
+     *
+     * @param resultSet A ResultSet instance.
+     * @throws SQLException if the ResultSet instance methods throw an exception.
+     */
     public Actor(ResultSet resultSet) throws SQLException {
         super(resultSet);
     }
