88511346eaf9c6f8ab0f26acbad00ffc7037e9d6
Maximilian Wendel <maximilian.wendel@mwtdev.se>
2021-05-20 09:22:17 +0200

    Removed unused customer constructor

--- src/main/java/org/library/account/Customer.java
+++ src/main/java/org/library/account/Customer.java
@@ -11,12 +11,6 @@ public class Customer extends Account {
         super();
     }
 
-    public Customer(Account account, CustomerType customerType) {
-        super(account.getId(), account.getGivenName(), account.getFamilyName(), account.getEmail(), account.getPhoneNumber());
-
-        this.customerType = customerType;
-    }
-
     public Customer(ResultSet resultSet) throws SQLException {
         super(resultSet.getLong("id"), resultSet.getString("given_name"), resultSet.getString("family_name"), resultSet.getString("email"), resultSet.getString("phone"));
 
