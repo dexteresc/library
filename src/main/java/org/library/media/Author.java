d49153edf53bb3532e535342207bc4d5a452b347
Maximilian Wendel <maximilian.wendel@mwtdev.se>
2021-05-20 16:30:10 +0200

    Added javadoc comments for author class

--- src/main/java/org/library/media/Author.java
+++ src/main/java/org/library/media/Author.java
@@ -3,8 +3,17 @@ package org.library.media;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+/**
+ * Author representing an author of a book.
+ */
 public class Author extends Person {
 
+    /**
+     * Creates a new author instance from a result set.
+     *
+     * @param resultSet A ResultSet instance.
+     * @throws SQLException if the ResultSet instance methods throw an exception.
+     */
     public Author(ResultSet resultSet) throws SQLException {
         super(resultSet);
     }
