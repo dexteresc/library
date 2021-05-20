143b41ae4b1bae99741898bff97a893057c2a76a
Maximilian Wendel <maximilian.wendel@mwtdev.se>
2021-05-20 09:32:32 +0200

    Added javadoc comments for customer type class

--- src/main/java/org/library/account/CustomerType.java
+++ src/main/java/org/library/account/CustomerType.java
@@ -3,17 +3,31 @@ package org.library.account;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+/**
+ * Customer type representation. (e.g. Student, Researcher, University Staff)
+ *
+ * @see Customer
+ */
 public class CustomerType {
     private Long id;
     private String typeName;
     private int maxNumberOfLoans;
 
-    public CustomerType(Long id, String typeName, int maxNumberOfLoans) {
+    /**
+     * Creates a new instance of customer type.
+     */
+    private CustomerType(Long id, String typeName, int maxNumberOfLoans) {
         this.id = id;
         this.typeName = typeName;
         this.maxNumberOfLoans = maxNumberOfLoans;
     }
 
+    /**
+     * Creates a new customer type instance from a result set.
+     *
+     * @param resultSet A ResultSet instance.
+     * @throws SQLException if the ResultSet instance methods throw an exception.
+     */
     public CustomerType(ResultSet resultSet) throws SQLException {
         this(resultSet.getLong("id"), resultSet.getString("type_name"), resultSet.getInt("max_concurrent_loans"));
     }
