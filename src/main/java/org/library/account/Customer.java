6ae28531ae796ab6711585d18e5e6ec5a9142e42
Maximilian Wendel <maximilian.wendel@mwtdev.se>
2021-05-20 09:24:18 +0200

    Added javadoc comments for customer class

--- src/main/java/org/library/account/Customer.java
+++ src/main/java/org/library/account/Customer.java
@@ -3,14 +3,28 @@ package org.library.account;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+/**
+ * Account representing a customer of the library system.
+ *
+ * @see Account
+ */
 public class Customer extends Account {
     private CustomerType customerType;
-    private Long numberOfActiveLoans;
+    private Long numberOfActiveLoans; // Pre-computed value
 
+    /**
+     * Creates a new, empty, customer instance.
+     */
     public Customer() {
         super();
     }
 
+    /**
+     * Creates a new customer instance from a result set.
+     *
+     * @param resultSet A ResultSet instance.
+     * @throws SQLException if the ResultSet instance methods throw an exception.
+     */
     public Customer(ResultSet resultSet) throws SQLException {
         super(resultSet.getLong("id"), resultSet.getString("given_name"), resultSet.getString("family_name"), resultSet.getString("email"), resultSet.getString("phone"));
 
