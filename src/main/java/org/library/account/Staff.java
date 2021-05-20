f9eb8b870739f25b125e28899c629509fa1adb6a
Maximilian Wendel <maximilian.wendel@mwtdev.se>
2021-05-20 09:35:07 +0200

    Added javadoc comments for staff class

--- src/main/java/org/library/account/Staff.java
+++ src/main/java/org/library/account/Staff.java
@@ -3,15 +3,27 @@ package org.library.account;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+/**
+ * Account representing a staff of the library system.
+ *
+ * @see Account
+ */
 public class Staff extends Account {
     private String role;
 
-    public Staff(Account account, String role) {
-        super(account.getId(), account.getGivenName(), account.getFamilyName(), account.getEmail(), account.getPhoneNumber());
-
-        this.role = role;
+    /**
+     * Creates a new, empty, staff instance.
+     */
+    public Staff() {
+        super();
     }
 
+    /**
+     * Creates a new staff instance from a result set.
+     *
+     * @param resultSet A ResultSet instance.
+     * @throws SQLException if the ResultSet instance methods throw an exception.
+     */
     public Staff(ResultSet resultSet) throws SQLException {
         super(resultSet.getLong("id"), resultSet.getString("given_name"), resultSet.getString("family_name"), resultSet.getString("email"), resultSet.getString("phone"));
 
