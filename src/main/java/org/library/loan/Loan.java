f93c8b89b456518ec9bb7e00e0bc7949cd64deb5
Maximilian Wendel <maximilian.wendel@mwtdev.se>
2021-05-20 09:56:55 +0200

    Added javadoc comments for loan class

--- src/main/java/org/library/loan/Loan.java
+++ src/main/java/org/library/loan/Loan.java
@@ -4,6 +4,12 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
 
+/**
+ * Loan representing a lease-type relationship between a media item and a customer.
+ *
+ * @see org.library.media.MediaItem
+ * @see org.library.account.Customer
+ */
 public class Loan {
     private Long id;
     private Long customerId;
@@ -12,7 +18,10 @@ public class Loan {
     private LocalDate returnBy;
     private LocalDate returnedAt;
 
-    public Loan(Long id, Long customerId, Long mediaItemId, LocalDate borrowedAt, LocalDate returnBy, LocalDate returnedAt) {
+    /**
+     * Creates a new loan instance.
+     */
+    protected Loan(Long id, Long customerId, Long mediaItemId, LocalDate borrowedAt, LocalDate returnBy, LocalDate returnedAt) {
         this.id = id;
         this.customerId = customerId;
         this.mediaItemId = mediaItemId;
@@ -21,6 +30,12 @@ public class Loan {
         this.returnedAt = returnedAt;
     }
 
+    /**
+     * Creates a new loan instance from a result set.
+     *
+     * @param resultSet A ResultSet instance.
+     * @throws SQLException if the ResultSet instance methods throw an exception.
+     */
     public Loan(ResultSet resultSet) throws SQLException {
         this(resultSet.getLong("id"), resultSet.getLong("customer_id"), resultSet.getLong("media_item_id"), resultSet.getObject("borrowed_at", LocalDate.class), resultSet.getObject("return_by", LocalDate.class), resultSet.getObject("returned_at", LocalDate.class));
     }
