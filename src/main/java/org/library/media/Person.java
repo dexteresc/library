553f02fad511e4c816174ba6526c6eb4a1e5dca7
Maximilian Wendel <maximilian.wendel@mwtdev.se>
2021-05-23 17:16:22 +0200

    Added javadoc comments for person

--- src/main/java/org/library/media/Person.java
+++ src/main/java/org/library/media/Person.java
@@ -4,20 +4,38 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Objects;
 
+/**
+ * Person representing an individual associated with a work.
+ *
+ * @see Actor
+ * @see Author
+ */
 public abstract class Person {
     private Long id;
     private String givenName;
     private String familyName;
 
+    /**
+     * Create a new, empty, person instance.
+     */
     public Person() {
     }
 
+    /**
+     * Creates a new person instance.
+     */
     protected Person(Long id, String givenName, String familyName) {
         this.id = id;
         this.givenName = givenName;
         this.familyName = familyName;
     }
 
+    /**
+     * Creates a new person instance from a result set.
+     *
+     * @param resultSet A ResultSet instance.
+     * @throws SQLException if the ResultSet instance methods throw an exception.
+     */
     public Person(ResultSet resultSet) throws SQLException {
         this(resultSet.getLong("id"), resultSet.getString("given_name"), resultSet.getString("family_name"));
     }
